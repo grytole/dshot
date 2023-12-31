808c2200c858a36ed7e8e795a8f059812b66182c
grytole <grytole@gmail.com>
2023-12-31 19:04:03 +0200

    working single channel solution

--- dshot.c
+++ dshot.c
@@ -10,20 +10,20 @@
 #define DSHOT_THROTTLE_BITS  (11)
 #define DSHOT_TELEMETRY_BITS (1)
 #define DSHOT_CHECKSUM_BITS  (4)
+#define DSHOT_GAP_BITS       (1)
 #define DSHOT_PACKET_BITS    (DSHOT_THROTTLE_BITS + DSHOT_TELEMETRY_BITS + DSHOT_CHECKSUM_BITS)
-#define DSHOT_GAP_BITS       (2)
 
 #define DSHOT_THROTTLE_MAX   (0x7FF)
 #define DSHOT_TELEMETRY_OFF  (0)
 
-#define DSHOT_LUT_SLOTS_PER_BIT (8)
-#define DSHOT_LUT_SLOT_BIT_0    (3)
-#define DSHOT_LUT_SLOT_BIT_1    (6)
-#define DSHOT_LUT_LEN           ((DSHOT_PACKET_BITS + DSHOT_GAP_BITS) * DSHOT_LUT_SLOTS_PER_BIT)
+#define DSHOT_LUT_BIT_0      (15) /* DSHOT150: 2.50us @ 6MHz */
+#define DSHOT_LUT_BIT_1      (30) /* DSHOT150: 5.00us @ 6MHz */
+#define DSHOT_LUT_BIT_PERIOD (40) /* DSHOT150: 6.67us @ 6MHz */
+#define DSHOT_LUT_LEN        (DSHOT_PACKET_BITS + DSHOT_GAP_BITS)
 
 static dshot_config_t dshot_config = {};
 static bool dshot_config_initialized = false;
-static uint32_t dshot_lut[DSHOT_LUT_LEN] = {};
+static uint8_t dshot_lut[DSHOT_LUT_LEN] = {};
 
 static uint8_t dshot_checksum(uint16_t packet);
 
@@ -39,49 +39,59 @@ static uint8_t dshot_checksum(uint16_t packet)
 bool dshot_init(dshot_config_t *config)
 {
   bool result = false;
+  uint16_t tim_prescaler = 0;
   uint16_t tim_period = 0;
 
   if (config)
   {
     if ((0 != config->speed) &&
         (0 != config->gpio) &&
-        (0 != config->pins) &&
+        (0 != config->pin) &&
         (0 != config->tim) &&
         (0 != config->dma) &&
         (0 != config->dma_channel))
     {
       dshot_config.speed = config->speed;
       dshot_config.gpio = config->gpio;
-      dshot_config.pins = config->pins;
+      dshot_config.pin = config->pin;
       dshot_config.tim = config->tim;
+      dshot_config.tim_channel = config->tim_channel;
       dshot_config.dma = config->dma;
       dshot_config.dma_channel = config->dma_channel;
       dshot_config_initialized = true;
 
-      tim_period = (rcc_get_timer_clk_freq(dshot_config.tim) / dshot_config.speed) - 1;
+      tim_prescaler = (rcc_get_timer_clk_freq(dshot_config.tim) / dshot_config.speed) - 1;
+      tim_period = DSHOT_LUT_BIT_PERIOD - 1;
 
-      /* configure gpio pins */
-      gpio_set_mode(dshot_config.gpio, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, dshot_config.pins);
+      /* configure gpio pin */
+      gpio_set_mode(dshot_config.gpio, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, dshot_config.pin);
+      gpio_clear(dshot_config.gpio, dshot_config.pin);
+
       /* configure dshot_config.dma */
       dma_channel_reset(dshot_config.dma, dshot_config.dma_channel);
-      dma_set_peripheral_address(dshot_config.dma, dshot_config.dma_channel, (uint32_t)(&(GPIO_BSRR(dshot_config.gpio))));
+      /* TODO: move dma_set_peripheral_address() to dshot_send() and use corresponding TIM_CCRx register for requested channel */
+      dma_set_peripheral_address(dshot_config.dma, dshot_config.dma_channel, (uint32_t)(&(TIM_CCR1(dshot_config.tim))));
       dma_set_memory_address(dshot_config.dma, dshot_config.dma_channel, (uint32_t)(dshot_lut));
-      dma_set_peripheral_size(dshot_config.dma, dshot_config.dma_channel, DMA_CCR_PSIZE_32BIT);
-      dma_set_memory_size(dshot_config.dma, dshot_config.dma_channel, DMA_CCR_MSIZE_32BIT);
+      dma_set_peripheral_size(dshot_config.dma, dshot_config.dma_channel, DMA_CCR_PSIZE_16BIT);
+      dma_set_memory_size(dshot_config.dma, dshot_config.dma_channel, DMA_CCR_MSIZE_8BIT);
       dma_enable_memory_increment_mode(dshot_config.dma, dshot_config.dma_channel);
       dma_disable_peripheral_increment_mode(dshot_config.dma, dshot_config.dma_channel);
       dma_set_read_from_memory(dshot_config.dma, dshot_config.dma_channel);
       dma_set_priority(dshot_config.dma, dshot_config.dma_channel, DMA_CCR_PL_HIGH);
+
       /* configure timer */
-      timer_set_clock_division(dshot_config.tim, TIM_CR1_CKD_CK_INT);
-      timer_set_alignment(dshot_config.tim, TIM_CR1_CMS_EDGE);
-      timer_direction_up(dshot_config.tim);
+      timer_set_mode(dshot_config.tim, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
       timer_enable_preload(dshot_config.tim);
+      timer_set_prescaler(dshot_config.tim, tim_prescaler);
       timer_set_period(dshot_config.tim, tim_period);
+      timer_enable_oc_output(dshot_config.tim, dshot_config.tim_channel);
+      timer_set_oc_mode(dshot_config.tim, dshot_config.tim_channel, TIM_OCM_PWM1);
+      timer_enable_oc_preload(dshot_config.tim, dshot_config.tim_channel);
       timer_enable_update_event(dshot_config.tim);
       timer_set_dma_on_update_event(dshot_config.tim);
       timer_enable_irq(dshot_config.tim, TIM_DIER_UDE);
       timer_enable_counter(dshot_config.tim);
+
       result = true;
     }
   }
@@ -89,54 +99,29 @@ bool dshot_init(dshot_config_t *config)
   return result;
 }
 
-bool dshot_set(uint16_t pin, uint16_t value)
+bool dshot_set(uint16_t value)
 {
   bool result = false;
   uint16_t packet = 0;
   uint8_t csum = 0;
   uint8_t i = 0;
-  uint16_t lut_offset = 0;
 
   if (dshot_config_initialized)
   {
-    if (pin & dshot_config.pins)
+    /* set and clamp throttle value */
+    packet = value & DSHOT_THROTTLE_MAX;
+    /* set telemetry bit to disabled */
+    packet = (packet << DSHOT_TELEMETRY_BITS) | DSHOT_TELEMETRY_OFF;
+    /* set packet checksum */
+    csum = dshot_checksum(packet);
+    packet = (packet << DSHOT_CHECKSUM_BITS) | csum;
+    /* set lookup table value */
+    for (i = 0; i < DSHOT_PACKET_BITS; i++)
     {
-      /* set and clamp throttle value */
-      packet = value & DSHOT_THROTTLE_MAX;
-      /* set telemetry bit to disabled */
-      packet = (packet << DSHOT_TELEMETRY_BITS) | DSHOT_TELEMETRY_OFF;
-      /* set packet checksum */
-      csum = dshot_checksum(packet);
-      packet = (packet << DSHOT_CHECKSUM_BITS) | csum;
-      /* prepare lookup table */
-      for (i = 0; i < DSHOT_PACKET_BITS; i++)
-      {
-        lut_offset = i * DSHOT_LUT_SLOTS_PER_BIT;
-        dshot_lut[lut_offset] |= pin;
-        if (packet & 0x8000)
-        {
-          dshot_lut[lut_offset + DSHOT_LUT_SLOT_BIT_0] &= ~(pin << 16);
-          dshot_lut[lut_offset + DSHOT_LUT_SLOT_BIT_1] |= (pin << 16);
-          //DBG /* remove set LO, add clear LO, add set HI, remove clear HI */
-          //DBG dshot_lut[lut_offset + DSHOT_LUT_SLOT_BIT_0] &= ~(pin);
-          //DBG dshot_lut[lut_offset + DSHOT_LUT_SLOT_BIT_0] |= (pin << 16);
-          //DBG dshot_lut[lut_offset + DSHOT_LUT_SLOT_BIT_1] |= pin;
-          //DBG dshot_lut[lut_offset + DSHOT_LUT_SLOT_BIT_1] &= ~(pin << 16);
-        }
-        else
-        {
-          dshot_lut[lut_offset + DSHOT_LUT_SLOT_BIT_1] &= ~(pin << 16);
-          dshot_lut[lut_offset + DSHOT_LUT_SLOT_BIT_0] |= (pin << 16);
-          //DBG /* add set LO, remove clear LO, remove set HI, add clear HI */
-          //DBG dshot_lut[lut_offset + DSHOT_LUT_SLOT_BIT_0] |= pin;
-          //DBG dshot_lut[lut_offset + DSHOT_LUT_SLOT_BIT_0] &= ~(pin << 16);
-          //DBG dshot_lut[lut_offset + DSHOT_LUT_SLOT_BIT_1] &= ~(pin);
-          //DBG dshot_lut[lut_offset + DSHOT_LUT_SLOT_BIT_1] |= (pin << 16);
-        }
-        packet <<= 1;
-      }
-      result = true;
+      dshot_lut[i] = (packet & 0x8000) ? DSHOT_LUT_BIT_1 : DSHOT_LUT_BIT_0;
+      packet <<= 1;
     }
+    result = true;
   }
 
   return result;
--- dshot.h
+++ dshot.h
@@ -5,22 +5,23 @@
 #include <stdbool.h>
 
 typedef enum {
-  DSHOT_150 = 1200000,
-  DSHOT_300 = 2400000,
-  DSHOT_600 = 4800000
+  DSHOT_150 =  6000000,
+  DSHOT_300 = 12000000,
+  DSHOT_600 = 24000000
 } dshot_baud_e;
 
 typedef struct {
-  dshot_baud_e speed;
-  uint32_t     gpio;
-  uint16_t     pins;
-  uint32_t     tim;
-  uint32_t     dma;
-  uint8_t      dma_channel;
+  uint32_t speed;
+  uint32_t gpio;
+  uint16_t pin;
+  uint32_t tim;
+  uint8_t  tim_channel;
+  uint32_t dma;
+  uint8_t  dma_channel;
 } dshot_config_t;
 
 bool dshot_init(dshot_config_t *config);
-bool dshot_set(uint16_t pin, uint16_t value);
+bool dshot_set(uint16_t value);
 bool dshot_send(void);
 
 #endif /* __DSHOT_H__ */
